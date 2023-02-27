283ee54baff6c425978a82dd1039e9e528c7c566
Vladyslav <vkadyrov@protonmail.com>
2023-02-26 19:01:00 -0500

    commit tree 232519f5b2898c25558239bbd919d9c516e46ad4

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+# require 'active_record'
+
+class Costume < ActiveRecord::Base
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,5 @@
 # Create your HauntedHouse class here
+# require 'active_record'
+
+class HauntedHouse < ActiveRecord::Base
+end
